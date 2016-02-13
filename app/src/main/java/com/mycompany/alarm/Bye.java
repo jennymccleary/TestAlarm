4c7044317bf50abd1f38eac48abcc3f99e6b636a
Jennifer McCleary <mccleary@mit.edu>
2016-02-13 13:51:46 -0500

    Initial commit

---
+++ app/src/main/java/com/mycompany/alarm/Bye.java
@@ -0,0 +1,15 @@
+package com.mycompany.alarm;
+
+/**
+ * Created by jennifermccleary on 2/13/16.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Bye extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bye_layout);
+    }
+}
