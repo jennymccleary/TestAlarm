e07de3feed3a45d0b22cd35393a3a96c1292c88a
jennyxu <jennyxu@dhcp-18-189-27-51.dyn.mit.edu>
2016-02-13 17:28:45 -0500

    Added onTimeChanged event so logs currenthour and currentminute

--- Alarm/app/src/main/java/com/mycompany/alarm/Alarm.java
+++ Alarm/app/src/main/java/com/mycompany/alarm/Alarm.java
@@ -2,6 +2,7 @@ package com.mycompany.alarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.AlarmManager;
@@ -19,29 +20,37 @@ import android.widget.TimePicker;
 import java.util.Calendar;
 
 public class Alarm extends AppCompatActivity {
-
+    private Intent i;
+    private Context c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_alarm);
-        Calendar t = Calendar.getInstance();
-        t.add(Calendar.SECOND, 15);
 
-        Intent i = new Intent(this, AlarmSound.class);
-        PendingIntent pending = PendingIntent.getActivity(this,1235, i, PendingIntent.FLAG_CANCEL_CURRENT);
-
-        AlarmManager alarm = (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+        i = new Intent(this, AlarmSound.class);
+        c = this;
 
         TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
-        calendar.set(Calendar.MINUTE, timePicker.getMinute());
-        alarm.set(AlarmManager.RTC_WAKEUP, t.getTimeInMillis(), pending);
+         timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Calendar t = Calendar.getInstance();
+                PendingIntent pending = PendingIntent.getActivity(c, 1235, i, PendingIntent.FLAG_CANCEL_CURRENT);
+                t.add(Calendar.SECOND, 15);
+                AlarmManager alarm = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                Log.e(Integer.toString(Calendar.HOUR_OF_DAY), Integer.toString(Calendar.HOUR_OF_DAY));
+                Log.e(Integer.toString(Calendar.MINUTE), Integer.toString(Calendar.MINUTE));
+
+                alarm.set(AlarmManager.RTC_WAKEUP, t.getTimeInMillis(), pending);
+
+            }
+        });
         //startActivity(intent);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
